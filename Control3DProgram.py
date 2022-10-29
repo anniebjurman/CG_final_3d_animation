8845a5642a5bcaad120d448231192f590b0efc96
Annie Bjurman <id19abn@cs.umu.se>
2022-10-29 18:29:40 +0000

    build space scene

--- Control3DProgram.py
+++ Control3DProgram.py
@@ -25,7 +25,7 @@ class GraphicsProgram3D:
 
         self.model_matrix = Matrices.ModelMatrix()
 
-        self.sphere_width = 40
+        self.sphere_width = 80
 
         self.projection_matrix = Matrices.ProjectionMatrix()
         self.projection_matrix.set_perspective(60, 1920/1080, 0.8, 150)
@@ -38,7 +38,7 @@ class GraphicsProgram3D:
         self.shader.set_view_matrix(self.view_matrix.get_matrix())
 
         # set light
-        self.shader.set_light_position(Base3DObjects.Point(20, 40, 0))
+        self.shader.set_light_position(Base3DObjects.Point(20, 50, -20))
 
         # General objects
         self.cube = Base3DObjects.Cube()
@@ -54,10 +54,10 @@ class GraphicsProgram3D:
         self.lin_motion = Motion.LinearMotion(Base3DObjects.Point(0, 5, -1), Base3DObjects.Point(0, 0, -1), 5.0, 10.0)
         self.model_pos_lin = Base3DObjects.Point(0,0,0)
 
-        self.bez_motion = Motion.BezierMotion(Base3DObjects.Point(0, 0, 0),
-                                                 Base3DObjects.Point(10, 0, -15),
-                                                 Base3DObjects.Point(20, 0, -15),
-                                                 Base3DObjects.Point(20, 0, 0),
+        self.bez_motion = Motion.BezierMotion(Base3DObjects.Point(-20, 1, 0),
+                                                 Base3DObjects.Point(-5, 10, -20),
+                                                 Base3DObjects.Point(15, 5, -20),
+                                                 Base3DObjects.Point(20, 1, 0),
                                                  5.0, 10.0)
         self.model_pos_bez = Base3DObjects.Point(0,0,0)
 
@@ -85,8 +85,13 @@ class GraphicsProgram3D:
 
         # textures
         self.texture_id_01 = Texture.load_texture(sys.path[0] + "/textures/tex_01.png")
-        self.texture_wood = Texture.load_texture(sys.path[0] + "/textures/tex_wooden_floor.jpeg")
-        self.texture_sphere = Texture.load_texture(sys.path[0] + "/textures/tex_sphere_5.jpeg")
+        self.texture_floor = Texture.load_texture(sys.path[0] + "/textures/tex_metallic_floor_2.jpeg")
+        self.texture_sphere = Texture.load_texture(sys.path[0] + "/textures/tex_sphere_6.png")
+        self.texture_box = Texture.load_texture(sys.path[0] + "/textures/tex_metallic.jpeg")
+        self.texture_mars = Texture.load_texture(sys.path[0] + "/textures/tex_mars.jpeg")
+        self.texture_mercury = Texture.load_texture(sys.path[0] + "/textures/tex_mercury.jpeg")
+        self.texture_jupiter = Texture.load_texture(sys.path[0] + "/textures/tex_jupiter.jpeg")
+        self.texture_moon = Texture.load_texture(sys.path[0] + "/textures/tex_moon.jpeg")
 
     def update(self):
         self.time_elapsed = pygame.time.get_ticks() / 1000
@@ -146,15 +151,14 @@ class GraphicsProgram3D:
         self.model_matrix.push_matrix()
         self.model_matrix.add_translation(self.sphere_width / 2, self.sphere_width / 2, - self.sphere_width / 2)
 
-        # Floor
-        self.shader.set_using_texture(1.0)
-        Texture.set_diffuse_tex(self.shader, self.texture_wood)
-        Texture.set_specular_tex(self.shader, self.texture_wood)
         self.draw_floor()
+        self.draw_mars()
+        self.draw_mercury()
+        self.draw_jupiter()
+        self.draw_moon()
 
         self.shader.set_using_texture(0.0)
         self.draw_model_person()
-        self.draw_model_ghost()
 
         self.model_matrix.pop_matrix()
         ######### Translated to middle of sphere #########
@@ -168,20 +172,6 @@ class GraphicsProgram3D:
         self.model_matrix.load_identity()
 
         pygame.display.flip()
-
-    def draw_sky_box(self):
-        width = 40
-        height = 20
-        self.model_matrix.push_matrix()
-        self.model_matrix.add_translation(width/2, height/2, -width/2)
-        self.model_matrix.add_scale(width, height, width)
-        self.shader.set_model_matrix(self.model_matrix.matrix)
-        self.shader.set_mat_diffuse(Base3DObjects.Color(1.0, 1.0, 1.0))
-        self.shader.set_mat_shine(13)
-        self.shader.set_mat_ambient(Base3DObjects.Color(0.5, 0.5, 0.5))
-        self.cube.set_vertices(self.shader)
-        self.cube.draw()
-        self.model_matrix.pop_matrix()
 
     def draw_lin_moving_cube(self):
         self.model_matrix.push_matrix()
@@ -208,7 +198,9 @@ class GraphicsProgram3D:
     def draw_floor(self):
         floor_dim = 10
         floor_thickness = 0.2
-
+        self.shader.set_using_texture(1.0)
+        Texture.set_diffuse_tex(self.shader, self.texture_floor)
+        Texture.set_specular_tex(self.shader, self.texture_floor)
         self.model_matrix.push_matrix()
         self.model_matrix.add_translation(y = - floor_thickness / 2)
         self.model_matrix.add_scale(floor_dim, floor_thickness, floor_dim)
@@ -228,9 +220,7 @@ class GraphicsProgram3D:
         self.model_matrix.add_scale(self.sphere_width, self.sphere_width, self.sphere_width)
         self.shader.set_model_matrix(self.model_matrix.matrix)
 
-        self.shader.set_mat_diffuse(Base3DObjects.Color(1, 1, 1))
-        self.shader.set_mat_shine(13)
-        self.shader.set_mat_ambient(Base3DObjects.Color(0.5, 0.5, 0.5))
+        self.shader.set_mat_ambient(Base3DObjects.Color(0.2, 0.2, 0.2))
 
         self.opt_sphere.set_vertices(self.shader)
         self.opt_sphere.draw()
@@ -245,18 +235,55 @@ class GraphicsProgram3D:
         self.obj_model_person.draw(self.shader)
         self.model_matrix.pop_matrix()
 
-    def draw_model_ghost(self):
+    def draw_mars(self):
+        self.shader.set_using_texture(1.0)
+        Texture.set_diffuse_tex(self.shader, self.texture_mars)
+        Texture.set_specular_tex(self.shader, self.texture_mars)
         self.model_matrix.push_matrix()
-        self.model_matrix.add_translation()
+        self.model_matrix.add_translation(x = -5, y = 10)
+        self.model_matrix.add_scale(1.5, 1.5, 1.5)
+        self.shader.set_model_matrix(self.model_matrix.matrix)
 
+        self.opt_sphere.set_vertices(self.shader)
+        self.opt_sphere.draw()
+        self.model_matrix.pop_matrix()
+
+    def draw_mercury(self):
+        self.shader.set_using_texture(1.0)
+        Texture.set_diffuse_tex(self.shader, self.texture_mercury)
+        Texture.set_specular_tex(self.shader, self.texture_mercury)
         self.model_matrix.push_matrix()
-        self.model_matrix.add_translation(self.model_pos_bez.x, self.model_pos_bez.y, self.model_pos_bez.z)
+        self.model_matrix.add_translation(x = -1, y = 10)
+        self.shader.set_model_matrix(self.model_matrix.matrix)
+
+        self.opt_sphere.set_vertices(self.shader)
+        self.opt_sphere.draw()
+        self.model_matrix.pop_matrix()
+
+    def draw_jupiter(self):
+        self.shader.set_using_texture(1.0)
+        Texture.set_diffuse_tex(self.shader, self.texture_jupiter)
+        Texture.set_specular_tex(self.shader, self.texture_jupiter)
+        self.model_matrix.push_matrix()
+        self.model_matrix.add_translation(x = 3, y = 10)
+        self.model_matrix.add_scale(2, 2, 2)
+        self.shader.set_model_matrix(self.model_matrix.matrix)
+
+        self.opt_sphere.set_vertices(self.shader)
+        self.opt_sphere.draw()
+        self.model_matrix.pop_matrix()
+
+    def draw_moon(self):
+        self.shader.set_using_texture(1.0)
+        Texture.set_diffuse_tex(self.shader, self.texture_moon)
+        Texture.set_specular_tex(self.shader, self.texture_moon)
+        self.model_matrix.push_matrix()
+        self.model_matrix.add_translation(y = 5)
         self.model_matrix.add_scale(0.5, 0.5, 0.5)
         self.shader.set_model_matrix(self.model_matrix.matrix)
 
-        self.obj_model_ghost.draw(self.shader)
-        self.model_matrix.pop_matrix()
-
+        self.opt_sphere.set_vertices(self.shader)
+        self.opt_sphere.draw()
         self.model_matrix.pop_matrix()
 
     def program_loop(self):
